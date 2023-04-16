d29939c3845ecc6092a7577276c014dc30183394
Murali ⚡️ <murali.nutalapatii@gmail.com>
2023-04-16 21:56:25 +0100

    use build spec

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,6 @@
-FROM node:alpine
-
-WORKDIR /express-app
-
-COPY package.json .
-
-RUN npm install
-
+FROM --platform=linux/amd64 node:18-alpine
+WORKDIR /app
 COPY . .
-
+RUN yarn install
+CMD ["node", "index.js"]
 EXPOSE 3000
-
-CMD [ "node", "index.js" ]
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("welcome to express app!!");
+  res.send("hey Murali!!!!!!");
 });
 
 app.get("/info", (req, res) => {
@@ -11,5 +11,5 @@ app.get("/info", (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log("listening");
+  console.log("listening on 3000");
 });
